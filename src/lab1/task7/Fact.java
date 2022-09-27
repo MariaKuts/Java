dcfedc23e60ca3538358132c8843fd96711aea68
ivc1-5 <kuts.2003@bk.ru>
2022-09-27 11:28:43 +0300

    Добавлена 1 практическая: задания №3-7

---
+++ src/lab1/task7/Fact.java
@@ -0,0 +1,17 @@
+package lab1.task7;
+import java.util.Scanner;
+public class Fact {
+    public static int getFactorial(int f) {
+        int result = 1;
+        for (int i = 1; i <= f; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите число, факториал которого необходимо найти: ");
+        int f = scan.nextInt();
+        System.out.println("Полученное значение: " + getFactorial(f));
+    }
+}
